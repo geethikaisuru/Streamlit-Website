de9935a32ee4bf915f2be7b8960ec84f5a2694cb
Geethika Isuru Sampath <35458073+geethikaisuru@users.noreply.github.com>
2024-11-26 17:39:20 +0530

    added Sidebar

--- Home.py
+++ Home.py
@@ -9,8 +9,8 @@ with col1:
 with col2:
     st.title("Geethika Isuru")
     content = """
-    I am an entrepreneur & a software engineer with a passion for data science and machine learning. I have experience in developing web applications, data analysis, and machine learning models. I am currently working as a software engineer at a software company in Sri Lanka. I have a bachelor's degree in ICT from the University of Sri Jayewardenepura. I am also a self-taught data scientist and machine learning engineer. I have completed several online courses in data science and machine learning. I am always eager to learn new technologies and improve my skills. I am looking for opportunities to work on challenging projects in data science and machine learning.
-                """
+                I am an entrepreneur & a software engineer with a passion for data science and machine learning. I have experience in developing web applications, data analysis, and machine learning models. I am currently working as a software engineer at a software company in Sri Lanka. I have a bachelor's degree in ICT from the University of Sri Jayewardenepura. I am also a self-taught data scientist and machine learning engineer. I have completed several online courses in data science and machine learning. I am always eager to learn new technologies and improve my skills. I am looking for opportunities to work on challenging projects in data science and machine learning.
+            """
     st.write(content)
 
 st.write("Below are some of the projects I have worked on.")
